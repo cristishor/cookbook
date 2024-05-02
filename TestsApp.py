ca4704362f09fad30bca8bec0b9fd27b41c68549
Cristian Chiriac <84506167+cristishor@users.noreply.github.com>
2024-05-02 19:27:10 +0300

    CRUD File rename

--- TestsApp.py
+++ TestsApp.py
@@ -1,4 +1,4 @@
-import Cookbook
+import crud_ops
 
 ### GLOBAL ENV VAR
 TEST_DB_1 = 'test001.db'
@@ -6,24 +6,24 @@ TEST_DB_1 = 'test001.db'
 def test_READ(doTest = 0):
     if doTest:
         # Init flags / variables
-        Cookbook._TEST_SIMULATION = 1
-        Cookbook._PRINT_CRUD_OP = 1
-        Cookbook._DEBUG_READ_DATA = 1
+        crud_ops._TEST_SIMULATION = 1
+        crud_ops._PRINT_CRUD_OP = 1
+        crud_ops._DEBUG_READ_DATA = 1
 
         # Call processes
-        Cookbook.fetchDataFromDB(TEST_DB_1)
+        crud_ops.fetchDataFromDB(TEST_DB_1)
 
         # Deinit flags / variables
-        Cookbook._TEST_SIMULATION = 0
-        Cookbook._PRINT_CRUD_OP = 0
-        Cookbook._DEBUG_READ_DATA = 0
+        crud_ops._TEST_SIMULATION = 0
+        crud_ops._PRINT_CRUD_OP = 0
+        crud_ops._DEBUG_READ_DATA = 0
 
 def test_CREATE(doTest = 0):
     if doTest:
         # Init flags / variables
-        Cookbook._TEST_SIMULATION = 1
-        Cookbook._PRINT_CRUD_OP = 1
-        Cookbook._DEBUG_CREATE_DATA = 1
+        crud_ops._TEST_SIMULATION = 1
+        crud_ops._PRINT_CRUD_OP = 1
+        crud_ops._DEBUG_CREATE_DATA = 1
 
         food_date = [
             'new_food', {'d':1,'m':1,'y':2024},
@@ -36,30 +36,30 @@ def test_CREATE(doTest = 0):
 
         # Call processes
         print('\n##### START TEST ######')
-        ALL_RECIPES = Cookbook.fetchDataFromDB(TEST_DB_1)
+        ALL_RECIPES = crud_ops.fetchDataFromDB(TEST_DB_1)
         for i in range(0, len(food_date), 2):
-            msg = Cookbook.sendNewOrEditedDataToDB(ALL_RECIPES, food_date[i], food_date[i+1])
+            msg = crud_ops.sendNewOrEditedDataToDB(ALL_RECIPES, food_date[i], food_date[i+1])
             print(msg)
 
         # Deinit flags / variables
-        Cookbook._TEST_SIMULATION = 0
-        Cookbook._PRINT_CRUD_OP = 0
-        Cookbook._DEBUG_CREATE_DATA = 0
+        crud_ops._TEST_SIMULATION = 0
+        crud_ops._PRINT_CRUD_OP = 0
+        crud_ops._DEBUG_CREATE_DATA = 0
                 
 def test_DELETE(doTest = 0):
     if doTest:
         # Init flags / variables
-        Cookbook._TEST_SIMULATION = 1
-        Cookbook._PRINT_CRUD_OP = 1
-        Cookbook._DEBUG_DELETE_DATA = 1
+        crud_ops._TEST_SIMULATION = 1
+        crud_ops._PRINT_CRUD_OP = 1
+        crud_ops._DEBUG_DELETE_DATA = 1
 
         # Call processes
 
 
         # Deinit flags / variables
-        Cookbook._TEST_SIMULATION = 0
-        Cookbook._PRINT_CRUD_OP = 0
-        Cookbook._DEBUG_DELETE_DATA = 0
+        crud_ops._TEST_SIMULATION = 0
+        crud_ops._PRINT_CRUD_OP = 0
+        crud_ops._DEBUG_DELETE_DATA = 0
 
 
 #### RUN TESTS ###
